736310a842127a48018d128a71dac6e84e906767
Jonathan Prieto <jonathan.prieto@me.com>
2019-09-17 22:19:18 -0500

    Martes Tarde

---
+++ cmake-build-debug/CMakeFiles/type-traits-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/type-traits-jonathanprieto99.dir/main.cpp.o"
+  "CMakeFiles/type-traits-jonathanprieto99.dir/test/tester.cpp.o"
+  "CMakeFiles/type-traits-jonathanprieto99.dir/mock/mocker.cpp.o"
+  "type-traits-jonathanprieto99.pdb"
+  "type-traits-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/type-traits-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
